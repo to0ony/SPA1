10544d3856459c7bf963a9d5ae9befa4ec77251d
to0ony <31554124+to0ony@users.noreply.github.com>
2023-11-07 13:04:43 +0100

    Finished 3 task

--- Zadaca 3/Zadaca3_3.cpp
+++ Zadaca 3/Zadaca3_3.cpp
@@ -3,8 +3,33 @@
 
 using namespace std;
 
+//Binary Search Tree Implementation
 
+int binarySearchTree(vector<int>& vec, int sizeOfVec, int x){
+    int minIndex = 0;
+    int maxIndex = sizeOfVec - 1;
+    while(minIndex <= maxIndex){
+        int midIndex = minIndex + (maxIndex - minIndex) / 2;
+        if(vec[midIndex] == x){
+            cout << "X is located on " << midIndex << " index!" << endl;
+            return midIndex;
+        }
+        if(vec[midIndex] < x){
+            minIndex += midIndex;
+        }
+        if(vec[midIndex] > x){
+            maxIndex -= midIndex;
+        }
+    }
+    cout << "X is not located in array!" << endl;
+    return -1;
+}
 
 int main(){
-
+vector<int> elements = {2,4,6,12,24,48};
+int size = elements.size();
+binarySearchTree(elements, size, 50);
+binarySearchTree(elements, size, 4);
+binarySearchTree(elements, size, 2);
+binarySearchTree(elements, size, 48);
 }
